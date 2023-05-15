58f5e8eb6fd603156bcbacc956c1f75f12fd4fdc
SMS-D01\JVFMatos <jvmatosfagundes@gmail.com>
2023-05-15 14:30:57 -0300

    Criação de telas

--- app-ged/next.config.js
+++ app-ged/next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/Login', // Página que você deseja definir como tela inicial
+        permanent: true,
+      },
+    ];
+  },
+};
